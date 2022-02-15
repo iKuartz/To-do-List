567f27aac27da827518408ce598e40e709efc2ea
Ivan Silva <ivankuartz@hotmail.com>
2022-02-15 13:11:04 -0300

    HTML plugin installed

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index_bundle.js',
   },
+  plugins: [new HtmlWebpackPlugin()],
 };
