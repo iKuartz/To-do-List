c1c9779b806c4ba9334031115d604b319960473d
Ivan Silva <ivankuartz@hotmail.com>
2022-02-15 14:13:17 -0300

    Webpack now fully configured, no errors. I will never touch this again.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import "./template.css";
 
 function component() {
   const element = document.createElement('div');
--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,16 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+    mode: 'development', 
+    entry: './src/index.js',
+    devServer: {
+    static: './dist',
+    }, 
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'index_bundle.js',
   },
-    plugins: [new HtmlWebpackPlugin()],
+    plugins: [new HtmlWebpackPlugin({template:'./src/template.html'})],
   module: {
     rules: [
       {
