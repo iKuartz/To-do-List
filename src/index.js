5c9185ccb4ea13c4f1e606a6b8ccaaf70624b689
Ivan Silva <ivankuartz@hotmail.com>
2022-02-15 18:25:28 -0300

    Class and function structure created

--- src/index.js
+++ src/index.js
@@ -10,10 +10,43 @@ let todoList = [
     index: 0,
   },]
 
-const listElements = document.querySelector('.to-do-list');
+class Todo_List {
+  constructor(li)
+    this.ulElement = li;
+}
+
+function addTask() {
+  const todoInput = document.querySelector('#taskInput').value;
+  const taskObject = {
+    description: todoInput,
+    completed: false,
+    index: todoList.length,
+    }
+}
+
+function check(a) {
+
+}
+
+function deleteElenent(b) {
+  
+}
+
+function dragNdrop(c) {
+
+}
 
 function generateHTML(details) {
   const template = `
+  <li>${description}</li>
   `;
   return template;
 }
+
+const listElements = document.querySelector('.to-do-list');
+
+myTodoList = new Todo_List(listSection);
+document.querySelector("#taskInput").addEventListener("click"), function () {
+  myTodoList.addTask()
+};
+
