23d773713b2cb2eac6dba73abb856df9fd23ae19
Ivan Silva <ivankuartz@hotmail.com>
2022-02-18 17:43:31 -0300

    next attempt

--- src/modules/deleteone.js
+++ src/modules/deleteone.js
@@ -3,5 +3,6 @@ export default function deleteOne(array, selectedId) {
   for (let i = 0; i < array.length; i += 1) {
     array[i].index = i + 1;
   }
+  // descriptive comment
   localStorage.setItem('todoList', JSON.stringify(array));
 }
