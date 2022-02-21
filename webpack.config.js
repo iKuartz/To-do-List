b1487f368146ba4666a739c7ccea9d004f8db017
Ivan Silva <ivankuartz@hotmail.com>
2022-02-21 18:02:09 -0300

    webpack correction

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: 'main.js',
+    filename: './main.js',
     publicPath: '',
   },
   plugins: [new HtmlWebpackPlugin({ template: './src/template.html' })],
